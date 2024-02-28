04648bc97337d15cf670169b6ce42d9c373afce3
reify-dhan-moonian <dan.moonian@gmail.com>
2024-02-28 08:10:07 -0700

    Added email provider

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -2,6 +2,8 @@ import { NextRequest, NextResponse } from "next/server";
 import { userProvider } from "@/app/providers/userProvider";
 import { User } from "@prisma/client";
 import * as bcrypt from "bcrypt";
+import { EmailTemplate, emailProvider } from "@/app/providers/emailProvider";
+import { getUserProfileName } from "../users/route";
 interface RegisterBody {
   user: Omit<User, "status" | "createdAt" | "updatedAt" | "stripeId">;
 }
@@ -14,6 +16,20 @@ export async function POST(req: NextRequest) {
       status: "PENDING",
       password: await bcrypt.hash(body.user.password, 10),
     });
+
+    // send new application email to admin
+    emailProvider.sendEmailWithTemplate({
+      emailTo: "admin@colexsoftwaresolutions.com",
+      emailTemplateId: EmailTemplate.NEW_APPLICATION_REQUEST,
+      emailParams: {
+        name: await getUserProfileName(user.id, user.role),
+        role: user.role,
+        websiteUrl: `${
+          process.env.WEBSITE_URL
+        }/admin/${user.role.toLowerCase()}`,
+      },
+    });
+
     return new NextResponse(JSON.stringify({ user, success: true }));
   } catch (error: any) {
     console.log(error);
