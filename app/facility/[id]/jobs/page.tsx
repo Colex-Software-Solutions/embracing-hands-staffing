7de8aa3e1aa91daa6a34b03be522e9a1db50b1b0
Dhaneshen1810 <dan.moonian@gmail.com>
2024-04-28 09:40:32 -0600

    Fixed google location script not loading

--- app/facility/[id]/jobs/page.tsx
+++ app/facility/[id]/jobs/page.tsx
@@ -27,7 +27,7 @@ export default async function JobsPage() {
     <>
       <Script
         src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places`}
-        strategy="beforeInteractive"
+        strategy="afterInteractive"
       />
       <div className="h-full flex-1 flex-col space-y-8 p-8 flex">
         <div className="flex items-center justify-between space-y-2">
