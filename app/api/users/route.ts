f0b25f5009e1b52066bcb1221c4b7574d3ea15d5
Ryan Thabet <thabet.abe@gmail.com>
2025-01-12 14:45:34 -0700

    fixed bugs in admin staff page

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -29,8 +29,17 @@ export async function POST(request: Request) {
   const body: RequestBody = await request.json();
 
   try {
-    const user = await userProvider.updateUserStatus(body.id, body.status);
+    const user = await userProvider.getUser(body.id);
+    if (!user) {
+      return new Response(
+        JSON.stringify({
+          success: false,
+          error: "Could not find the user in the system",
+        })
+      );
+    }
     const userProfileName = await getUserProfileName(body.id, user.role);
+    await userProvider.updateUserStatus(body.id, body.status);
 
     if (body.status === "APPROVED") {
       await emailProvider.sendEmailWithTemplate({
